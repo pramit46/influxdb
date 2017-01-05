57c9c607345256d5521e3d7ba76cafadf24109ab
pramit46 <pramit46@yahoo.com>
2017-01-05 19:23:29 +0530

    Update python_script_to_automate_influxdb_point_creation.py

--- python_script_to_automate_influxdb_point_creation.py
+++ python_script_to_automate_influxdb_point_creation.py
@@ -5,18 +5,18 @@ import datetime
 import random
 
 
-#errorcount, APP=EM, type=IISlog, region=aws-e errorcode=<<errorcode>>,errorcount=<<number>> <<timestamp>>
-#errorcount,app=em, type=iislog errorcode=503,errorcount=160 1477300521
+#errorcount, APP=EMITE, type=IISlog, region=awseast errorcode=<<errorcode>>,errorcount=<<number>> <<timestamp>>
+#errorcount,app=emite, type=iislog errorcode=503,errorcount=160 1477300521
 
 
 
 filename='ErrorCountDataFile.txt'
 #target = open(filename, 'w')
-for x in xrange(0,500):
+for x in xrange(0,1):
 
         #target = open(filename, 'w')
 
-        rand=(random.randint(0,100))
+        rand=(random.randint(0,500))
         errorcode_rand=(random.randint(500,503))
         errorcode=errorcode_rand
 
@@ -32,26 +32,27 @@ for x in xrange(0,500):
         value = 'errorcount'+'='+(str(rand))
         #print(tag)
 
-        tag = 'app'+'='+'emite'+','+'type'+'='+'iislog'+','+'errorcode'+'='+str(errorcode)
+        tag = 'app'+'='+'emite'+','+'tech'+'='+'IIS'+','+'errorcode'+'='+str(errorcode)
         #print(value)
 
         point_val = measurement+','+tag+' '+value+' '
+
 
         #+' '+str(int(timestamp))    # +str(1422568543702900261)
         print(point_val)
 
         #target.write(point_val)
-		#target.write("\n")
+        #target.write("\n")
 
         #target.close()
 
         url = "http://10.121.48.24:8086/write?db=pramit"
         #file_data = open(filename, 'rb').read()
         #fs = {'file' : open(filename,'rb')}
-        r = requests.post(url,data=point_val)
-        print(r.text)
-        print(r.json)
+        #r = requests.post(url,data=point_val)
+        #print(r.text)
+        #print(r.json)
 
 
-#curl -i -XPOST 'http://localhost:8086/write?db=pramit' --data-binary @DataFile.txt
-#errorcount,app=em, type=iislog errorcode=503,errorcount=160 1477300521
+#curl -i -XPOST 'http://localhost:8086/write?db=pramit&precision=ms' --data-binary @DataFile.txt
+#errorcount,app=emite, type=iislog errorcode=503,errorcount=160 1477300521
